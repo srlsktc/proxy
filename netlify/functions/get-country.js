2b74b84a2c25a709d4137575ae120ca39bb6f3aa
Vitaly Mamaev <minvitlc@icloud.com>
2024-08-29 14:56:11 +0700

    countries

---
+++ netlify/functions/get-country.js
@@ -0,0 +1,73 @@
+const fetch = require('node-fetch');
+const ApiSigner = require('../utils/signer');
+const { serializeQueryParams } = require('../utils/serializer');
+
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type,x-api-key,x-api-signature',
+  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+};
+
+const handler = async (event) => {
+  try {
+    console.log('Http method:', event.httpMethod, event.queryStringParameters);
+
+    // Handle preflight request
+    if (event.httpMethod === 'OPTIONS') {
+      return {
+        statusCode: 200,
+        headers: corsHeaders,
+      };
+    }
+
+    // Get the IP from the request, defaulting to the requester's IP if none provided
+    const ip = event.queryStringParameters?.ip || event.requestContext.identity.sourceIp;
+
+    if (!ip) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: 'IP address is required' }),
+        headers: {
+          ...corsHeaders,
+          'Content-Type': 'application/json',
+        },
+      };
+    }
+
+    // Send request to IP info API
+    const apiUrl = `https://freeipapi.com/api/json/${ip}`;
+    const response = await fetch(apiUrl);
+    const responseBody = await response.json();
+
+    console.log('Raw Response:', responseBody);
+
+    // Format the response to the needed format
+    const formattedResponse = {
+      countryCode: responseBody.countryCode,
+      displayName: responseBody.countryName,
+    };
+
+    console.log('Formatted Response:', JSON.stringify(formattedResponse));
+    
+    return {
+      statusCode: response.status,
+      body: JSON.stringify(formattedResponse), // Convert formatted response to a string
+      headers: {
+        ...corsHeaders,
+        'Content-Type': 'application/json',
+      },
+    };
+  } catch (error) {
+    console.log('catch error', JSON.stringify({ error: error.toString() }));
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.toString() }),
+      headers: {
+        ...corsHeaders,
+        'Content-Type': 'application/json',
+      },
+    };
+  }
+};
+
+module.exports = { handler };
